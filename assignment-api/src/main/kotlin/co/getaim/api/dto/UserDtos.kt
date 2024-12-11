c58dd930f0c0c1ac0a0b5acda51cc00e7e684178
Woongbae Yun <devbelly@naver.com>
2024-12-11 22:03:01 +0900

    feat: UserDto 추가

---
+++ assignment-api/src/main/kotlin/co/getaim/api/dto/UserDtos.kt
@@ -0,0 +1,26 @@
+package co.getaim.api.dto
+
+import co.getaim.domain.user.UserDomain
+
+data class SignUpUserCommand(
+    val userId: String,
+    val username: String,
+    val password: String,
+)
+
+data class LoginUserCommand(
+    val userId: String,
+    val password: String,
+)
+
+data class SignUpUserResponse(
+    val userId: String,
+    val username: String,
+) {
+    constructor(user: UserDomain) : this(user.userId, user.username)
+}
+
+data class LoginUserResponse(
+    val accessToken: String,
+    val refreshToken: String,
+)
