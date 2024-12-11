274b553d664bdf0e4b36607a09b837fb156b922e
Woongbae Yun <devbelly@naver.com>
2024-12-11 22:05:27 +0900

    feat: Jwt 토큰 발급 기능 구현

---
+++ assignment-api/src/main/kotlin/co/getaim/security/JwtTokenProvider.kt
@@ -0,0 +1,75 @@
+package co.getaim.security
+
+import co.getaim.exception.token.TokenException
+import co.getaim.exception.token.TokenExceptionType.*
+import io.jsonwebtoken.ExpiredJwtException
+import io.jsonwebtoken.Jwts
+import io.jsonwebtoken.MalformedJwtException
+import io.jsonwebtoken.UnsupportedJwtException
+import io.jsonwebtoken.security.Keys
+import io.jsonwebtoken.security.SignatureException
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.stereotype.Component
+import java.util.*
+import javax.crypto.SecretKey
+
+@Component
+class JwtTokenProvider(
+    @Value("\${jwt.secret}") private val secretKey: String,
+) {
+    private var signingKey: SecretKey = secretKey.toByteArray().let {
+        Keys.hmacShaKeyFor(it)
+    }
+
+    companion object {
+        const val ACCESS_TOKEN_EXPIRATION_MILLISECONDS: Long = 1000 * 60 * 60 * 1 // 1시간
+        const val REFRESH_TOKEN_EXPIRATION_MILLISECONDS: Long = 1000 * 60 * 60 * 24 * 14 // 2주
+    }
+
+    fun createAccessToken(payload: String): String {
+        return createToken(payload, ACCESS_TOKEN_EXPIRATION_MILLISECONDS)
+    }
+
+    fun createRefreshToken(payload: String): String {
+        return createToken(payload, REFRESH_TOKEN_EXPIRATION_MILLISECONDS)
+    }
+
+    fun createToken(payload: String, validity: Long): String {
+        return try {
+            Jwts.builder()
+                .setSubject(payload)
+                .setExpiration(Date(System.currentTimeMillis() + validity))
+                .signWith(signingKey)
+                .compact()
+        } catch (e: IllegalArgumentException) {
+            throw TokenException(CANNOT_CREATE_TOKEN)
+        }
+    }
+
+    fun getSubject(token: String): String {
+        return getClaimsJws(token)
+            .body
+            .subject
+    }
+
+    fun validateToken(token: String) {
+        try {
+            getClaimsJws(token)
+        } catch (e: SignatureException) {
+            throw TokenException(INVALID_TOKEN)
+        } catch (e: MalformedJwtException) {
+            throw TokenException(WRONG_TOKEN_TYPE)
+        } catch (e: ExpiredJwtException) {
+            throw TokenException(EXPIRED_TOKEN)
+        } catch (e: UnsupportedJwtException) {
+            throw TokenException(UNSUPPORTED_TOKEN)
+        } catch (e: IllegalArgumentException) {
+            throw TokenException(NOT_FOUND_TOKEN)
+        }
+    }
+
+    private fun getClaimsJws(token: String) = Jwts.parserBuilder()
+        .setSigningKey(signingKey.encoded)
+        .build()
+        .parseClaimsJws(token)
+}
