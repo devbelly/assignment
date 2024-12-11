e16003539176a825b712efca1996d7956f0382c2
Woongbae Yun <devbelly@naver.com>
2024-12-11 22:05:59 +0900

    feat: 커스텀 로거 구현

---
+++ common/src/main/kotlin/co/getaim/common/utils/logger.kt
@@ -0,0 +1,9 @@
+package co.getaim.common.utils
+
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
+
+
+inline fun <reified T> T.logger(): Logger {
+    return LoggerFactory.getLogger(T::class.java)
+}
