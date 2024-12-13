742d764416bc2419fc94ad992d0983fc9e54751f
Woongbae Yun <devbelly@naver.com>
2024-12-13 11:44:37 +0900

    feat: ResolverConfig 추가

---
+++ assignment-api/src/main/kotlin/co/getaim/config/AuthenticationConfig.kt
@@ -0,0 +1,18 @@
+package co.getaim.config
+
+import co.getaim.security.AuthTokenResolver
+import co.getaim.security.LoginUserResolver
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.method.support.HandlerMethodArgumentResolver
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
+
+@Configuration
+class AuthenticationConfig(
+    private val loginUserResolver: LoginUserResolver,
+    private val authTokenResolver: AuthTokenResolver,
+) : WebMvcConfigurer {
+    override fun addArgumentResolvers(resolvers: MutableList<HandlerMethodArgumentResolver>) {
+        resolvers.add(loginUserResolver)
+        resolvers.add(authTokenResolver)
+    }
+}
