9642021f8884d5cf13bb8adca31745ef75001d38
Woongbae Yun <devbelly@naver.com>
2024-12-13 13:10:46 +0900

    feat: 로그인 로그 AOP 기능 구현

---
+++ assignment-api/src/main/kotlin/co/getaim/security/LoginAop.kt
@@ -0,0 +1,33 @@
+package co.getaim.security
+
+import co.getaim.api.dto.LoginUserResponse
+import co.getaim.common.utils.logger
+import org.aspectj.lang.ProceedingJoinPoint
+import org.aspectj.lang.annotation.Around
+import org.aspectj.lang.annotation.Aspect
+import org.aspectj.lang.annotation.Pointcut
+import org.springframework.http.ResponseEntity
+import org.springframework.stereotype.Component
+
+
+@Aspect
+@Component
+class LoginAop {
+
+    private val log = logger()
+
+    @Pointcut("execution(* co.getaim.api.controller.UserController.login(..))")
+    fun loginPointcut() {
+
+    }
+
+    @Around("loginPointcut()")
+    fun logLogin(joinPoint: ProceedingJoinPoint): Any {
+        val result = joinPoint.proceed()
+        val username = ((result as? ResponseEntity<*>)?.body as? LoginUserResponse)?.username
+
+        log.info("사용자가 로그인 했습니다. : $username")
+
+        return result
+    }
+}
