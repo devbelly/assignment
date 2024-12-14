221c23f7ae909b798b65443bcb3305577ef27151
Woongbae Yun <devbelly@naver.com>
2024-12-14 19:45:07 +0900

    chore: starter-jdbc 의존성 추가

--- assignment-api/build.gradle.kts
+++ assignment-api/build.gradle.kts
@@ -18,6 +18,9 @@ dependencies {
 
     implementation("org.springframework.boot:spring-boot-starter-aop")
 
+    //transaction
+    implementation("org.springframework.boot:spring-boot-starter-jdbc:3.4.0")
+
     compileOnly("io.jsonwebtoken:jjwt-api:0.11.2")
     runtimeOnly("io.jsonwebtoken:jjwt-impl:0.11.2")
     runtimeOnly("io.jsonwebtoken:jjwt-jackson:0.11.2")
