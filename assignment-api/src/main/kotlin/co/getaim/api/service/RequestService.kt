cf1dc35898a289181b2eb6af01ca4a61c9deb654
Woongbae Yun <devbelly@naver.com>
2024-12-16 12:37:53 +0900

    feat: 요청 조회 & 업데이트 기능 구현

--- assignment-api/src/main/kotlin/co/getaim/api/service/RequestService.kt
+++ assignment-api/src/main/kotlin/co/getaim/api/service/RequestService.kt
@@ -1,6 +1,9 @@
 package co.getaim.api.service
 
 import co.getaim.api.dto.CreateRequestCommand
+import co.getaim.domain.request.RequestDomain
+import co.getaim.domain.request.RequestStatus
+import co.getaim.domain.request.RiskType
 import co.getaim.storage.persistence.mysql.persistence.RequestPersistService
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
@@ -10,7 +13,15 @@ import org.springframework.transaction.annotation.Transactional
 class RequestService(
     private val requestPersistService: RequestPersistService
 ) {
+    fun find(requestId: Long) : RequestDomain {
+        return requestPersistService.findById(requestId)
+    }
+
     fun create(userId: Long, request: CreateRequestCommand) {
         requestPersistService.save(userId, request.type)
+    }
+
+    fun update(requestId: Long, type: RiskType, status: RequestStatus) {
+        requestPersistService.update(requestId, type, status)
     }
 }
