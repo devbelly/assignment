b1709c4cc4133e51e0f179c202b974723ab4c386
Woongbae Yun <devbelly@naver.com>
2024-12-16 12:34:57 +0900

    feat: 위험도에 따라 가용가능한 금액 계산 로직 구현

--- domain/src/main/kotlin/co/getaim/domain/request/RequestDomain.kt
+++ domain/src/main/kotlin/co/getaim/domain/request/RequestDomain.kt
@@ -1,8 +1,14 @@
 package co.getaim.domain.request
 
+import java.math.BigDecimal
+
 class RequestDomain(
     val id: Long,
     val userId: Long,
     val status: RequestStatus,
     val riskType: RiskType
-)
+){
+    fun maximumMoney(amount: BigDecimal): BigDecimal {
+        return amount.multiply(BigDecimal(riskType.value))
+    }
+}
